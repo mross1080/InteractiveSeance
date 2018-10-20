84fc35446f3db4766bfb72dc9d84e27fa5dc47dd
Brent Bailey <bb2906@nyu.edu>
2018-10-20 13:56:56 -0400

    remove unnecessary variables from ino script

--- seance_middleware_2/seance_middleware_2.ino
+++ seance_middleware_2/seance_middleware_2.ino
@@ -1,17 +1,8 @@
 #include <FastLED.h>
 #include <Servo.h>
 Servo myServo; // create servo object to control a servo
-
+// all credit to Mark Kriegman and FastLED
 FASTLED_USING_NAMESPACE
-
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
 
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
@@ -20,7 +11,7 @@ FASTLED_USING_NAMESPACE
 #define DATA_PIN    3
 #define SERVO_PIN   5
 //#define CLK_PIN   4
-#define LED_TYPE    WS2811
+#define LED_TYPE    NEOPIXEL
 #define COLOR_ORDER GRB
 #define NUM_LEDS    60
 uint8_t sat = 0;
@@ -28,10 +19,7 @@ int upOrDown = 0;
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  120
-boolean in_layer_1 = false;
-boolean in_layer_2 = false;
-boolean in_layer_3 = false;
+// is this useful? #define FRAMES_PER_SECOND  120
 
 void setup() {
   Serial.begin(9600);
@@ -72,26 +60,17 @@ void loop() {
     
   // put your main code here, to run repeatedly:
   if (intFromSerial == 1) {
-   in_layer_1 = true;
-   in_layer_2 = false;
-   in_layer_2 = false;
    initialState();
    myServo.write(60);
    delay(15);
   }
   
   if (intFromSerial == 2) {
-    in_layer_1 = false;
-    in_layer_2 = true;
-    in_layer_2 = false;
     speedUp();
     myServo.write(120);
     delay(15);
   }
   if (intFromSerial == 3) {
-    in_layer_1 = false;
-    in_layer_2 = false;
-    in_layer_2 = true;
     redRing();
     myServo.write(180);
     delay(15);
